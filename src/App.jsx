8f186ac2e8290f2c05a5f7f5301e227ebd482ad5
Sasikumar183 <142684386+Sasikumar183@users.noreply.github.com>
2024-07-19 15:04:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { WeatherDetails } from './assets/components/WeatherDetails';
 function App() {
   return (
     <>
-      <div className='h-[100vh] bg-cover' style={{ backgroundImage: `url(${bgim})` }}>
+      <div className='h-full bg-cover' style={{ backgroundImage: `url(${bgim})` }}>
         <WeatherDetails />
       </div>
     </>
